65b5fb6157317add4471fb06c61ffc8f8afc2cfc
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-02-03 06:36:54 +0200

    added validations

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-
+const express = require('express')//import express
+const app = express()//create an express app
+var bodyParser = require('body-parser')//import body-parser
 const users = [{
     id: 1,
     name:'Tresor',
@@ -12,6 +12,13 @@ const users = [{
     age:16,
     gender:'Male'
 }]
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 //api to get all users
 
@@ -29,7 +36,27 @@ app.get('/api/users/:id',(req,res)=>{
     return res.status(404).send(`User with id(${req.params.id}) was not found`)
 
 })
-//api to get user
+//api to create user
+app.post('/api/users',(req,res)=>{
+    console.log (typeof(req.body.name))
+    //Joi
+    let error = !req.body.name?'name is required' : !req.body.age?'age is required':!req.body.gender?'Gender is required':''
+    if(error != '')
+        return res.status(400).send(error)
+        let valid_gender = ['Male','Female']
+error = typeof(req.body.name) != "string"?'name must be a string' : typeof req.body.age != 'number' ?'age must be a number':!valid_gender.includes(req.body.gender)?`Gender is must be ${valid_gender.join()}`:''
+    if(error != '')
+        return res.status(400).send(error)
+    
+    let user = {
+        id: users.length + 1,
+        name:req.body.name,
+        age:req.body.age,
+        gender:req.body.gender
+    }
+    users.push(user)
+    return res.status(201).send(user)
+})
 app.listen(3000,()=>{
     console.log("Yooo Precieux,server is running ...")
 })
